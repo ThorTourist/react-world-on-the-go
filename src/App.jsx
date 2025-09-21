0f3d0af17d79876caf5c6ba5542b6f61983d7c59
Thor Tourist <shahidulu.du.geb@gmail.com>
2025-09-21 21:41:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+
+
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Countries in the world !!! </h1>
+        </div>
+    );
+};
+
+export default Countries;
